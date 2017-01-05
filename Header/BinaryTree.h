70761b5d797db9fedb0536f52e7341cd3a58d5e2
Eloquence4 <zelin11@live.com>
2017-01-05 20:54:02 +0200

    Initial Tree Structure (no implementations yet)

--- Header/BinaryTree.h
+++ Header/BinaryTree.h
@@ -2,12 +2,48 @@
 #define BinaryTreeDec
 
 template <typename VarType>
+struct Tree_Node
+{
+    VarType Data;
+    Tree_Node* Left;
+    Tree_Node* Right;
+
+    Tree_Node(const VarType& _Data = VarType(), Tree_Node* _Left = nullptr, Tree_Node* _Right = nullptr)
+        : Data(_Data)
+        , Left(_Left)
+        , Right(_Right)
+    { }
+};
+
+template <typename VarType>
 class BinaryTree
 {
     public:
+    // G4
+    BinaryTree();
+    ~BinaryTree();
+    BinaryTree(const BinaryTree<VarType>& source);
+    BinaryTree<VarType>& operator=(const BinaryTree<VarType>& rhs);
+    /////
+
+    void add(const VarType& what);
+    void remove(const VarType& key);
+
+#include "../HPP/BinaryTreeIterator.hpp"
+
+    TreeIterator top() const;
+
+    TreeIterator search(const VarType& key) const;
 
     private:
 
+    void add(const VarType& what, Tree_Node<VarType>* node);
+    void remove_node(Tree_Node<VarType>* cur);
+    void copy_node(Tree_Node<VarType> destination, Tree_Node<VarType>* source);
+
+    Tree_Node<VarType>* top;
 };
+
+#include "../HPP/BinaryTree.hpp"
 
 #endif // BinaryTreeDec
